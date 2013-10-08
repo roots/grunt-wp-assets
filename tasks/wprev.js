bdf78bdbcbc70d0729cfa309180b000cbca065aa
Hariadi Hinta <diperakui@yahoo.com>
2013-10-08 22:47:12 +0800

    async

--- tasks/wprev.js
+++ tasks/wprev.js
@@ -10,38 +10,41 @@
  */
 'use strict';
 
-var fs = require('fs')
-  , path = require('path')
-  , crypto = require('crypto');
+var fs = require('fs'),
+    path = require('path'),
+    crypto = require('crypto');
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('wprev', 'WordPress assets revving', function() {
 
-    var dest = this.data.dest
-      , options = this.options({ encoding: 'utf8'
-                              , algorithm: 'md5'
-                              , format: true
-                              , length: 4
-                              , rename: false
-                            });
+    var dest = this.data.dest,
+        options = this.options({
+        encoding: 'utf8',
+        algorithm: 'md5',
+        format: true,
+        length: 4,
+        rename: false
+      });
 
-    this.files.forEach(function(files) {
+    grunt.util.async.forEach(this.files, function (files, next) {
 
-      files.src.forEach(function (file) {
-
-        if (file.length === 0) {
-          grunt.log.warn('Source does not exists');
-          return;
+      files.src.filter(function(file) {
+        if (!grunt.file.exists(file)) {
+          grunt.log.warn('Source file "' + file + '" not found.');
+          return false;
+        } else {
+          return true;
         }
+      }).forEach(function(file) {
 
-        var basename = path.basename
-          , name = basename(file)
-          , content = grunt.file.read(file)
-          , hash = crypto.createHash(options.algorithm).update(content, options.encoding).digest('hex')
-          , suffix = hash.slice(0, options.length)
-          , ext = path.extname(file)
-          , newName = options.format ? [suffix, basename(file, ext), ext.slice(1)].join('.') : [basename(file, ext), suffix, ext.slice(1)].join('.');
+        var basename = path.basename,
+            name = basename(file),
+            content = grunt.file.read(file),
+            hash = crypto.createHash(options.algorithm).update(content, options.encoding).digest('hex'),
+            suffix = hash.slice(0, options.length),
+            ext = path.extname(file),
+            newName = options.format ? [suffix, basename(file, ext), ext.slice(1)].join('.') : [basename(file, ext), suffix, ext.slice(1)].join('.');
 
         // Copy/rename file base on hash and format
         var resultPath = path.resolve(path.dirname(file), newName);
@@ -52,15 +55,16 @@ module.exports = function(grunt) {
         }
 
         // Get target, find and change references assets to new hashed.
-        var wpcontent = grunt.file.read(dest)
-          , match = new RegExp('[a-z0-9]{'+ options.length +'}.' + name, "g")
-          , re = ( match.test(wpcontent) ) ? match : new RegExp(name, "g");
-        wpcontent = wpcontent.replace(re, newName);
+        var wpcontent = grunt.file.read(dest),
+            match = new RegExp('[a-z0-9]{'+ options.length +'}.' + name, "g"),
+            re = ( match.test(wpcontent) ) ? match : new RegExp(name, "g");
+            wpcontent = wpcontent.replace(re, newName);
 
-        grunt.file.write(dest, wpcontent);
-        var status = (options.rename) ? ' rename' : ' change';
-        grunt.log.writeln('  ' + file.grey + status + ' to ' + newName.green);
+      grunt.file.write(dest, wpcontent);
+      var status = (options.rename) ? ' rename' : ' change';
+      grunt.log.writeln('  ' + file.grey + status + ' to ' + newName.green);
       });
-    });
+      next();
+    }, this.async());
   });
 };
