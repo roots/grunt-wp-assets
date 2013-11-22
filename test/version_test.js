b9b8d30b06875f56644c6add0ce3858e493e8b52
Hariadi Hinta <diperakui@yahoo.com>
2013-11-22 10:29:44 +0800

    Update tests
    Adding hash function so filename more dynamic. No need to update test filename everytime we update to the latest main.min.css and scripts.min.js
    
    Update css and js test to latest Roots (04efa7b)

--- test/version_test.js
+++ test/version_test.js
@@ -6,19 +6,32 @@ var assert = require('assert');
 var css = "test/fixtures/assets/css/main.min.css";
 var js = "test/fixtures/assets/js/scripts.min.js";
 
+var hashed = function(filepath, algorithm, length) {
+  var crypto = require('crypto');
+
+  algorithm = algorithm || 'md5';
+  length = length || 4;
+
+  var hash = crypto.createHash(algorithm).update(fs.readFileSync(filepath)).digest('hex');
+  var suffix = hash.slice(0, length);
+  var ext = path.extname(filepath)
+  return [suffix, path.basename(filepath, ext), ext.slice(1)].join('.');
+};
+console.log(hashed(css));
+
 describe('version task', function () {
 
   describe("without options should", function() {
 
     it('revision css', function () {
       var original = fs.statSync(css).size;
-      var revisioned= fs.statSync('test/fixtures/assets/css/c9f2.main.min.css').size;
+      var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css)).size;
       assert(revisioned === original);
     });
 
     it('revision js', function () {
       var original = fs.statSync(js).size;
-      var revisioned= fs.statSync('test/fixtures/assets/js/2a3e.scripts.min.js').size;
+      var revisioned= fs.statSync('test/fixtures/assets/js/' + hashed(js)).size;
       assert(revisioned === original);
     });
 
@@ -28,13 +41,13 @@ describe('version task', function () {
 
     it('revision css: {algorithm: \'sha1\', length: 8, rename: true}', function () {
       var original = fs.statSync(css).size;
-      var revisioned= fs.statSync('test/fixtures/assets/css/5934af8d.main.min.css').size;
+      var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css, 'sha1', 8)).size;
       assert(revisioned === original);
     });
 
     it('revision js: {algorithm: \'sha1\', length: 8, rename: true}', function () {
       var original = fs.statSync(js).size;
-      var revisioned= fs.statSync('test/fixtures/assets/js/2a3e700c.scripts.min.js').size;
+      var revisioned= fs.statSync('test/fixtures/assets/js/' + hashed(js, 'sha1', 8)).size;
       assert(revisioned === original);
     });
 
@@ -45,7 +58,7 @@ describe('version task', function () {
       it('accept option: length: 8', function () {
 
           var original = fs.statSync(css).size;
-          var revisioned= fs.statSync('test/fixtures/assets/css/c9f21557.main.min.css').size;
+          var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css, 'md5', 8)).size;
           assert(revisioned === original);
 
       });
