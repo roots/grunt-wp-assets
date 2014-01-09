3d12e8ade63bc5bdab69005a29c08486d9596165
Ben Zörb <ben@sommerlaune.com>
2014-01-09 23:46:30 +0100

    changed tab style matching master

--- tasks/version.js
+++ tasks/version.js
@@ -11,92 +11,92 @@
 'use strict';
 
 var fs = require('fs'),
-	path = require('path'),
-	crypto = require('crypto'),
-	async = require('async');
+  path = require('path'),
+  crypto = require('crypto'),
+  async = require('async');
 
 module.exports = function (grunt) {
 
-	grunt.registerMultiTask('version', 'WordPress assets revving', function () {
+  grunt.registerMultiTask('version', 'WordPress assets revving', function () {
 
-		var dest = this.data.dest;
-		var options = this.options({
-			encoding: 'utf8',
-			algorithm: 'md5',
-			format: true,
-			length: 8,
-			rename: false,
-			querystring: {}
-		});
-		var querystring = (options.querystring.style && options.querystring.script) ? true : false;
+    var dest = this.data.dest;
+    var options = this.options({
+      encoding: 'utf8',
+      algorithm: 'md5',
+      format: true,
+      length: 8,
+      rename: false,
+      querystring: {}
+    });
+    var querystring = (options.querystring.style && options.querystring.script) ? true : false;
 
-		async.forEach(this.files, function (files, next) {
+    async.forEach(this.files, function (files, next) {
 
-			files.src.filter(function (file) {
-				if (!grunt.file.exists(file)) {
-					grunt.log.warn('Source file "' + file + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			}).forEach(function (file) {
+      files.src.filter(function (file) {
+        if (!grunt.file.exists(file)) {
+          grunt.log.warn('Source file "' + file + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).forEach(function (file) {
 
-					var basename = path.basename,
-						name = basename(file),
-						ext = path.extname(file),
-						namepart = path.basename(name, ext),
-						content = grunt.file.read(file),
-						hash = crypto.createHash(options.algorithm).update(content, options.encoding).digest('hex'),
-						suffix = hash.slice(0, options.length),
-						newName = options.format ? [suffix, basename(file, ext), ext.slice(1)].join('.') : [basename(file, ext), suffix, ext.slice(1)].join('.');
+          var basename = path.basename,
+            name = basename(file),
+            ext = path.extname(file),
+            namepart = path.basename(name, ext),
+            content = grunt.file.read(file),
+            hash = crypto.createHash(options.algorithm).update(content, options.encoding).digest('hex'),
+            suffix = hash.slice(0, options.length),
+            newName = options.format ? [suffix, basename(file, ext), ext.slice(1)].join('.') : [basename(file, ext), suffix, ext.slice(1)].join('.');
 
-					// Get target, find and change references assets to new hashed.
-					var wpcontent = grunt.file.read(dest), match, re;
+          // Get target, find and change references assets to new hashed.
+          var wpcontent = grunt.file.read(dest), match, re;
 
-					if (querystring) {
+          if (querystring) {
 
-						if (ext === '.css') {
+            if (ext === '.css') {
 
-							re = new RegExp('(wp_enqueue_style\\(' + '\'' + options.querystring.style + '\'' + ',(\\s*[^,]+,){2})\\s*[^\\)]+\\);');
-							newName = '$1 ' + '\'' + suffix + '\'' + ');';
+              re = new RegExp('(wp_enqueue_style\\(' + '\'' + options.querystring.style + '\'' + ',(\\s*[^,]+,){2})\\s*[^\\)]+\\);');
+              newName = '$1 ' + '\'' + suffix + '\'' + ');';
 
-						} else if (ext === '.js') {
+            } else if (ext === '.js') {
 
-							re = new RegExp('(wp_register_script\\(' + '\'' + options.querystring.script + '\'' + ',(\\s*[^,]+,){2})\\s*[^,]+,\\s*([^\\)]+)\\);');
-							newName = '$1 ' + '\'' + suffix + '\'' + ', ' + '$3);';
+              re = new RegExp('(wp_register_script\\(' + '\'' + options.querystring.script + '\'' + ',(\\s*[^,]+,){2})\\s*[^,]+,\\s*([^\\)]+)\\);');
+              newName = '$1 ' + '\'' + suffix + '\'' + ', ' + '$3);';
 
-						}
+            }
 
-						wpcontent = wpcontent.replace(re, newName);
-						grunt.log.writeln('  ' + dest.grey + ' update to ' + name.green + ' (' + suffix.grey + ')');
-					} else {
+            wpcontent = wpcontent.replace(re, newName);
+            grunt.log.writeln('  ' + dest.grey + ' update to ' + name.green + ' (' + suffix.grey + ')');
+          } else {
 
-						// Copy/rename file base on hash and format
-						var resultPath = path.resolve(path.dirname(file), newName);
-						if (options.rename) {
-							fs.renameSync(file, resultPath);
-						} else {
-							grunt.file.copy(file, resultPath);
-						}
+            // Copy/rename file base on hash and format
+            var resultPath = path.resolve(path.dirname(file), newName);
+            if (options.rename) {
+              fs.renameSync(file, resultPath);
+            } else {
+              grunt.file.copy(file, resultPath);
+            }
 
-						// Make sure $media for css and $ver for js is null
-						// Ref: wp_enqueue_style( $handle, $src, $deps, $ver, $media )
-						wpcontent = wpcontent.replace('\'' + hash + '\'', 'null').replace('\'' + suffix + '\'', 'null');
+            // Make sure $media for css and $ver for js is null
+            // Ref: wp_enqueue_style( $handle, $src, $deps, $ver, $media )
+            wpcontent = wpcontent.replace('\'' + hash + '\'', 'null').replace('\'' + suffix + '\'', 'null');
 
-						match = options.format ?
-							new RegExp('[a-z0-9]{' + options.length + '}.' + name, 'g') :
-							new RegExp(namepart + '.[a-z0-9]{' + options.length + '}' + ext, 'g');
+            match = options.format ?
+              new RegExp('[a-z0-9]{' + options.length + '}.' + name, 'g') :
+              new RegExp(namepart + '.[a-z0-9]{' + options.length + '}' + ext, 'g');
 
-						re = ( match.test(wpcontent) ) ? match : new RegExp(name, 'g');
-						wpcontent = wpcontent.replace(re, newName);
-						var status = (options.rename) ? ' rename' : ' change';
-						grunt.log.writeln('  ' + file.grey + status + ' to ' + newName.green);
-					}
+            re = ( match.test(wpcontent) ) ? match : new RegExp(name, 'g');
+            wpcontent = wpcontent.replace(re, newName);
+            var status = (options.rename) ? ' rename' : ' change';
+            grunt.log.writeln('  ' + file.grey + status + ' to ' + newName.green);
+          }
 
-					grunt.file.write(dest, wpcontent);
+          grunt.file.write(dest, wpcontent);
 
-				});
-			next();
-		}, this.async());
-	});
+        });
+      next();
+    }, this.async());
+  });
 };
--- test/version_test.js
+++ test/version_test.js
@@ -6,88 +6,88 @@ var assert = require('assert');
 var css = "test/fixtures/assets/css/main.min.css";
 var js = "test/fixtures/assets/js/scripts.min.js";
 
-var hashed = function(filepath, algorithm, length,format) {
+var hashed = function (filepath, algorithm, length, format) {
   var crypto = require('crypto');
 
   algorithm = algorithm || 'md5';
   length = length || 8;
-	format = typeof format === 'undefined' ? true : !!format;
+  format = typeof format === 'undefined' ? true : !!format;
 
   var hash = crypto.createHash(algorithm).update(fs.readFileSync(filepath)).digest('hex');
   var suffix = hash.slice(0, length);
   var ext = path.extname(filepath)
   return format ?
-	  [suffix, path.basename(filepath, ext), ext.slice(1)].join('.') :
-	  [path.basename(filepath, ext), suffix, ext.slice(1)].join('.');
+    [suffix, path.basename(filepath, ext), ext.slice(1)].join('.') :
+    [path.basename(filepath, ext), suffix, ext.slice(1)].join('.');
 };
 
 console.log(hashed(css));
 
 describe('version task', function () {
 
-  describe("without options should", function() {
+  describe("without options should", function () {
 
     it('revision css', function () {
       var original = fs.statSync(css).size;
-      var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css)).size;
+      var revisioned = fs.statSync('test/fixtures/assets/css/' + hashed(css)).size;
       assert(revisioned === original);
     });
 
     it('revision js', function () {
       var original = fs.statSync(js).size;
-      var revisioned= fs.statSync('test/fixtures/assets/js/' + hashed(js)).size;
+      var revisioned = fs.statSync('test/fixtures/assets/js/' + hashed(js)).size;
       assert(revisioned === original);
     });
 
   });
 
-  describe("with options should", function() {
+  describe("with options should", function () {
 
     it('revision css: {algorithm: \'sha1\', length: 8, rename: true}', function () {
       var original = fs.statSync(css).size;
-      var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css, 'sha1', 8)).size;
+      var revisioned = fs.statSync('test/fixtures/assets/css/' + hashed(css, 'sha1', 8)).size;
       assert(revisioned === original);
     });
 
     it('revision js: {algorithm: \'sha1\', length: 8, rename: true}', function () {
       var original = fs.statSync(js).size;
-      var revisioned= fs.statSync('test/fixtures/assets/js/' + hashed(js, 'sha1', 8)).size;
+      var revisioned = fs.statSync('test/fixtures/assets/js/' + hashed(js, 'sha1', 8)).size;
       assert(revisioned === original);
     });
 
   });
 
-  describe("when using Roots querystring disable", function() {
+  describe("when using Roots querystring disable", function () {
     if (path.extname(css) === '.css') {
       it('accept option: length: 8', function () {
 
-          var original = fs.statSync(css).size;
-          var revisioned= fs.statSync('test/fixtures/assets/css/' + hashed(css, 'md5', 8)).size;
-          assert(revisioned === original);
+        var original = fs.statSync(css).size;
+        var revisioned = fs.statSync('test/fixtures/assets/css/' + hashed(css, 'md5', 8)).size;
+        assert(revisioned === original);
 
       });
     }
   });
 
-	describe("replacement from file.{hash}.ext", function() {
-		if (path.extname(css) === '.css') {
-			it('has no scripts.min.01010101.js or main.min.01010101.css', function () {
+  describe("replacement from file.{hash}.ext", function () {
+    if (path.extname(css) === '.css') {
+      it('has no scripts.min.01010101.js or main.min.01010101.css', function () {
 
-				var modified = fs.readFileSync('test/fixtures/lib/scripts3.php', {encoding: 'utf8'});
+        var modified = fs.readFileSync('test/fixtures/lib/scripts3.php', {encoding: 'utf8'});
 
-				var oldjs = (new RegExp('scripts.min.01010101.js','g')).test(modified);
-				var oldcss = (new RegExp('main.min.01010101.css','g')).test(modified);
-				var newjs = (new RegExp(hashed(js, 'sha1', 8,false),'g')).test(modified);
-				var newcss = (new RegExp(hashed(css, 'sha1', 8,false),'g')).test(modified);
+        var oldjs = (new RegExp('scripts.min.01010101.js', 'g')).test(modified);
+        var oldcss = (new RegExp('main.min.01010101.css', 'g')).test(modified);
+        var newjs = (new RegExp(hashed(js, 'sha1', 8, false), 'g')).test(modified);
+        var newcss = (new RegExp(hashed(css, 'sha1', 8, false), 'g')).test(modified);
 
 
-				assert(oldjs === false);
-				assert(oldcss === false);
-				assert(newjs === true);
-				assert(newcss === true);
+        assert(oldjs === false);
+        assert(oldcss === false);
+        assert(newjs === true);
+        assert(newcss === true);
 
-			});
-		}
-	});
+      });
+    }
+  });
 
 });
